e1db1bd1ef73edc598cde51270613d296b99b271
southvalhalla <ah.alejo25@gmail.com>
2023-02-28 12:29:25 -0500

    Add functionality for add product to the table

--- src/app/page/product/new-product/new-product.component.ts
+++ src/app/page/product/new-product/new-product.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Category } from 'src/app/class/category.model';
+import { Product } from 'src/app/class/product.model';
+import { CategoryService } from 'src/app/service/category.service';
+import { ProductService } from 'src/app/service/product.service';
 
 @Component({
   selector: 'app-new-product',
@@ -6,9 +10,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./new-product.component.css']
 })
 export class NewProductComponent {
+  
+  categoryOption:Category[] = [];
+
+  constructor (private productService:ProductService ,private categoryService:CategoryService) {}
 
   CreateProduct(){
-
-    console.log('HOLA');
+    let product = new Product(this.product,this.trademark,this.categorySelected,this.date,this.description,this.price);
+    this.productService.newProduct(product);
+    alert('producto creado');
+    window.location.href = '/products';
   }
+
+  ngOnInit () {
+    this.categoryService.LoadCategories().subscribe(categories=>{
+      console.log(categories);
+      this.categoryOption = Object.values(categories);
+      this.categoryService.setCategories(this.categoryOption);
+    })
+  }
+
+  prod:string = '';
+
+  product:string = '';
+  trademark:string = '';
+  categorySelected:string = '';
+  date:string = '';
+  description:string = '';
+  price:number = 0;
 }
--- src/app/service/product.service.ts
+++ src/app/service/product.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { Product } from '../class/product.model';
+import { DataService } from './data.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  constructor() { }
+  constructor(private dataServise:DataService) { }
+
+  productArray:Product[] = [];
+
+  setProducts (products:Product[]) {
+    this.productArray = products;
+  }
+
+  loadProducts () {
+    return this.dataServise.GetProducts();
+  }
+
+  newProduct (product:Product) {
+    this.productArray.push(product);
+    this.dataServise.SendProducts(this.productArray);
+  }
 }
