28cc69e910c5b917489718f2cf8c3bd887568b6a
southvalhalla <ah.alejo25@gmail.com>
2023-02-28 12:13:53 -0500

    ADD: models

---
+++ src/app/class/product.model.ts
@@ -0,0 +1,18 @@
+export class Product {
+
+    constructor (name_prod:string, trademark:string, category:string, date:string, description_prod:string, price:number) {
+        this.name_prod = name_prod;
+        this.trademark = trademark;
+        this.category = category;
+        this.date = date;
+        this.description_prod = description_prod;
+        this.price = price;
+    }
+
+    name_prod:string = "";
+    trademark:string = "";
+    category:string = "";
+    date:string = "";
+    description_prod:string = "";
+    price:number = 0;
+}
