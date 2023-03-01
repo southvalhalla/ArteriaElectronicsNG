617a3c44881dec923e0b5a357ca3287062123065
southvalhalla <ah.alejo25@gmail.com>
2023-03-01 10:23:47 -0500

    Eliminate innecesary line

--- src/app/page/category/new-category/new-category.component.ts
+++ src/app/page/category/new-category/new-category.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { CategoryService } from 'src/app/service/category.service';
-import { ActivatedRoute, Route } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 import { Category } from 'src/app/class/category.model';
 
 @Component({
